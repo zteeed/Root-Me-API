12d5086db32785ddc4aace3bfae283ac69578864
zteeed <zteeed@minet.net>
2019-02-01 18:04:42 +0100

    Improve commit fa89b1e862d68db0207ff109404678d91ab7d3f8 in agreement with bonnetn

--- api/api/parser/stats.py
+++ api/api/parser/stats.py
@@ -7,17 +7,18 @@ from api.parser.exceptions import RootMeParsingError
 def extract_stats(txt):
     """ Evolution du Score """
     pattern = '<script type="text/javascript">(.*?)</script>'
-    result = re.findall(pattern, txt)
-    if not result:
+    stats_data = re.findall(pattern, txt)
+    if not stats_data:
         raise RootMeParsingError("Could not parse stats data about a user.")
-    result = ''.join(result)
-    pattern = 'evolution_data_total.push\(new Array\("(.*?)",(\d+), "(.*?)", "(.*?)"\)\)'
-    pattern += 'validation_totale\[(\d+)\]\+\=1;'
-    challenges_solved = re.findall(pattern, result)
 
+    stats_data = ''.join(stats_data)
+    pattern = ('evolution_data_total.push\(new Array\("(.*?)",(\d+), "(.*?)", "(.*?)"\)\)'
+               'validation_totale\[(\d+)\]\+\=1;')
+    challenges_solved = re.findall(pattern, result)
     result = []
-    for id, challenge_solved in enumerate(challenges_solved):
-        (date, score_at_date, name, path, difficulty) = challenge_solved
+
+    for challenge_solved in challenges_solved:
+        date, score_at_date, name, path, difficulty = challenge_solved
         challenge = {
             'name': unescape(name),
             'score_at_date': score_at_date,
@@ -26,4 +27,5 @@ def extract_stats(txt):
             'difficulty': difficulty,
         }
         result.append(challenge)
+
     return result
