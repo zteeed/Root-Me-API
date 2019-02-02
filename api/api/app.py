15e30edb9bf9fb5b832d04553d5f75c6df0c1517
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2019-02-02 23:36:14 +0100

    Remove caching

--- api/api/app.py
+++ api/api/app.py
@@ -2,9 +2,6 @@
 from logging.config import dictConfig
 
 from flask import Flask
-from flask_caching import Cache
-
-from api.constants import CACHE_TIMEOUT
 
 dictConfig({
     'version': 1,
@@ -22,4 +19,3 @@ dictConfig({
     }
 })
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': CACHE_TIMEOUT})
--- api/main.py
+++ api/main.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import math
-
 from flask import redirect, jsonify
 
-from api import repeater
-from api.app import app, cache
-from api.cache_wrapper import cached
-from api.constants import REFRESH_CACHE_INTERVAL, AUTHORS, ENDPOINTS, GITHUB_ACCOUNTS
+from api.app import app
+from api.constants import AUTHORS, ENDPOINTS, GITHUB_ACCOUNTS
 from api.http_interface.challenges import get_all_challenges
-from api.http_interface.profile import get_user_profile
 from api.http_interface.contributions import get_user_contributions
-from api.http_interface.details import get_user_details
 from api.http_interface.ctf import get_user_ctf
+from api.http_interface.details import get_user_details
+from api.http_interface.profile import get_user_profile
 from api.http_interface.stats import get_user_stats
 
 
 @app.route("/")
-@cached(timeout=math.inf)  # Never timeout, this is static.
 def root():
     return jsonify(title="Root-Me-API",
                    authors=AUTHORS,
@@ -28,7 +23,7 @@ def root():
 
 @app.route("/challenges")
 def challenges():
-    return jsonify(get_all_challenges_cached())
+    return jsonify(get_all_challenges())
 
 
 @app.route("/<username>")
@@ -37,46 +32,29 @@ def get_user(username):
 
 
 @app.route('/<username>/profile')
-@cache.memoize(timeout=10)
 def get_profile(username):
     return jsonify(get_user_profile(username))
 
 
 @app.route('/<username>/contributions')
-@cache.memoize(timeout=10)
 def get_contributions(username):
     return jsonify(get_user_contributions(username))
 
 
 @app.route('/<username>/details')
-@cache.memoize(timeout=10)
 def get_score(username):
     return jsonify(get_user_details(username))
 
 
 @app.route('/<username>/ctf')
-@cache.memoize(timeout=10)
 def get_ctf(username):
     return jsonify(get_user_ctf(username))
 
 
 @app.route('/<username>/stats')
-@cache.memoize(timeout=10)
 def get_stats(username):
     return jsonify(get_user_stats(username))
 
 
-@cached(key_prefix="challenges")
-def get_all_challenges_cached():
-    return get_all_challenges()
-
-
-def update_endpoints():
-    get_all_challenges_cached(cache_refresh=True)
-    app.logger.info("/challenges cache updated")
-
-
 if __name__ == "__main__":
-    repeater.start(update_endpoints, REFRESH_CACHE_INTERVAL)
     app.run(host='0.0.0.0', debug=True, use_reloader=False)
-    repeater.stop()
