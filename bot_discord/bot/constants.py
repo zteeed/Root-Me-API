c2ded0c66ac67f879306fec3c6f6716ae10a0aac
zteeed <zteeed@minet.net>
2019-02-05 02:02:58 +0000

    Add static values

---
+++ bot_discord/bot/constants.py
@@ -0,0 +1,11 @@
+bot_channel = 'root-me-news'
+emoji1 = 'dab'
+emoji2 = 'ok_hand'
+emoji3 = 'thinking'
+emoji4 = 'thinking'
+emoji5 = 'thumbsdown'
+token = 'token'
+""" Private API """
+URL = 'http://localhost:8001/v1'
+""" Public API """
+URL = 'http://localhost:8001/v1'
