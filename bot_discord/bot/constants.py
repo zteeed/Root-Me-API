eefbd178d3290bf112648e41692a846009a069e0
zteeed <zteeed@minet.net>
2019-02-06 23:39:05 +0000

    Add more static values

--- bot_discord/bot/constants.py
+++ bot_discord/bot/constants.py
@@ -4,6 +4,8 @@ emoji2 = 'ok_hand'
 emoji3 = 'thinking'
 emoji4 = 'thinking'
 emoji5 = 'thumbsdown'
+limit_size = 1000
+medals = [':first_place:', ':second_place:', ':third_place:']
 token = 'token'
 """ Private API """
 URL = 'http://localhost:8001/v1'
