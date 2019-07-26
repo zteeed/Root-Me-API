deb8dc5b3d0b8b1222e8d2b69859c4e7add92b07
zteeed <zteeed@minet.net>
2019-07-26 20:47:42 +0000

    Lint main

--- bot_discord/main.py
+++ bot_discord/main.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
 
-import asyncio, sys
-import discord
-from discord.ext import commands
+import asyncio
+import sys
 
-from bot.colors import grey, red, green, yellow, blue, purple, cyan
+import bot.display.embed as disp
+from bot.colors import red, green
 from bot.constants import token
 from bot.manage.discord_data import get_channel
-import bot.display.embed as disp
+from discord.ext import commands
 
 
 class RootMeBot():
 
-
     def __init__(self):
         """ Discord Bot to catch RootMe events made by zTeeed """
-        self.bot = commands.Bot(command_prefix = '!')
+        self.bot = commands.Bot(command_prefix='!')
         self.channel = None
         self.lock = False
-
 
     async def cron(self):
         await self.bot.wait_until_ready()
@@ -30,79 +28,66 @@ class RootMeBot():
                 red('RootMeBot locked')
             await asyncio.sleep(5)
 
-
     def catch(self):
         @self.bot.event
         async def on_ready():
             self.channel = get_channel(self.bot)
             await disp.ready(self)
 
-        
-        @self.bot.command(description = 'Add a user to team into database.') 
+        @self.bot.command(description='Add a user to team into database.')
         async def add_user(*args):
             """ <username> """
             await disp.add_user(self, args)
 
-
-        @self.bot.command(description = 'Remove a user from team in database.') 
+        @self.bot.command(description='Remove a user from team in database.')
         async def remove_user(*args):
             """ <username> """
             await disp.remove_user(self, args)
 
-
-        @self.bot.command(description = 'Show list of users from team.') 
+        @self.bot.command(description='Show list of users from team.')
         async def scoreboard():
             """ """
             await disp.scoreboard(self)
 
-
-        @self.bot.command(description = 'Show list of categories.') 
+        @self.bot.command(description='Show list of categories.')
         async def categories():
             """ """
             await disp.categories(self)
 
-
-        @self.bot.command(description = 'Show list of challenges from a category.') 
+        @self.bot.command(description='Show list of challenges from a category.')
         async def category(*args):
             """ <category> """
             await disp.category(self, args)
 
-
-        @self.bot.command(pass_context=True, description = 'Return who solved a specific challenge.') 
+        @self.bot.command(pass_context=True, description='Return who solved a specific challenge.')
         async def who_solved(ctx):
             """ <challenge> """
             await disp.who_solved(self, ctx)
-        
 
-        @self.bot.command(description = 'Return challenges solved grouped by users for last week.') 
+        @self.bot.command(description='Return challenges solved grouped by users for last week.')
         async def week(*args):
             """ (<username>) """
             await disp.week(self, args)
 
-
-        @self.bot.command(description = 'Return challenges solved grouped by users for last day.') 
+        @self.bot.command(description='Return challenges solved grouped by users for last day.')
         async def today(*args):
             """ (<username>) """
             await disp.today(self, args)
 
-
-        @self.bot.command(description = 'Return difference of solved challenges between two users.') 
+        @self.bot.command(description='Return difference of solved challenges between two users.')
         async def diff(*args):
             """ <username1> <username2> """
             await disp.diff(self, args)
 
-
-        @self.bot.command(description = 'Return difference of solved challenges between a user and all team.') 
+        @self.bot.command(description='Return difference of solved challenges between a user and all team.')
         async def diff_with(*args):
             """ <username> """
             await disp.diff_with(self, args)
 
-
-        @self.bot.command(description = 'Flush all data from bot channel excepted events') 
+        @self.bot.command(description='Flush all data from bot channel excepted events')
         async def flush(*args):
             """ """
             await disp.flush(self)
-
 
     def start(self):
         if token == 'token':
