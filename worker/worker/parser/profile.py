5167931cef141a5eda54ac3ea3f51c922cf44255
zteeed <zteeed@minet.net>
2019-08-01 22:32:59 +0000

    Remove a non-necessary if statement

--- worker/worker/parser/profile.py
+++ worker/worker/parser/profile.py
@@ -17,7 +17,7 @@ def extract_pseudo(r):
 def extract_score(r):
     tree = html.fromstring(r.content)
     result = tree.xpath('//ul[@class="spip"]/li[contains(., "Score")]/span/text()')
-    result = [int(score) for score in result if score.isdigit()]
+    result = [int(score) for score in result]
     if not result and not result[0]:  # result can be equal to [''] with this xpath search
         raise RootMeParsingError()
     return result[0]
