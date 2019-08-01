9f490345c5e684a788f4e06cbce52a25f87901dd
zteeed <zteeed@minet.net>
2019-08-01 01:00:28 +0000

    Use all functions to set values in redis

--- worker/main.py
+++ worker/main.py
@@ -1,59 +1,21 @@
-from flask import redirect, jsonify
-
-from api.app import app
-from api.constants import AUTHORS, GITHUB_ACCOUNTS
-from api.http_interface.challenges import get_all_challenges
-from api.http_interface.contributions import get_user_contributions
-from api.http_interface.ctf import get_user_ctf
-from api.http_interface.details import get_user_details
-from api.http_interface.profile import get_user_profile
-from api.http_interface.stats import get_user_stats
+from worker.redis_interface.challenges import set_all_challenges
+from worker.redis_interface.contributions import set_user_contributions
+from worker.redis_interface.ctf import set_user_ctf
+from worker.redis_interface.details import set_user_details
+from worker.redis_interface.profile import set_user_profile
+from worker.redis_interface.stats import set_user_stats
 
 
-@app.route("/")
-def root():
-    return redirect('/v1', code=302)
+def main():
+    users = ['zTeeed-115405']
+    set_all_challenges()
+    for username in users:
+        set_user_profile(username)
+        set_user_contributions(username)
+        set_user_details(username)
+        set_user_ctf(username)
+        set_user_stats(username)
 
 
-@app.route("/v1")
-def root_v1():
-    return jsonify(title="Root-Me-API", authors=AUTHORS, follow_us=GITHUB_ACCOUNTS)
-
-
-@app.route("/v1/challenges")
-def challenges():
-    return jsonify(get_all_challenges())
-
-
-@app.route("/v1/<string:username>")
-def get_user(username):
-    return redirect(f'/v1/{username}/profile', code=302)
-
-
-@app.route('/v1/<string:username>/profile')
-def get_profile(username):
-    return jsonify(get_user_profile(username))
-
-
-@app.route('/v1/<string:username>/contributions')
-def get_contributions(username):
-    return jsonify(get_user_contributions(username))
-
-
-@app.route('/v1/<string:username>/details')
-def get_score(username):
-    return jsonify(get_user_details(username))
-
-
-@app.route('/v1/<string:username>/ctf')
-def get_ctf(username):
-    return jsonify(get_user_ctf(username))
-
-
-@app.route('/v1/<string:username>/stats')
-def get_stats(username):
-    return jsonify(get_user_stats(username))
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+    main()
