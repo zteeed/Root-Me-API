358249e1650a5fe11d579155053fdfe4e28eec0a
zteeed <zteeed@minet.net>
2019-08-03 00:20:41 +0000

    Replace regex by xpath to fetch contributions

--- worker/worker/parser/contributions.py
+++ worker/worker/parser/contributions.py
@@ -1,24 +1,58 @@
-import re
-from html import unescape
+from lxml import html
 
 
-def extract_contributions(txt):
-    pattern = ('<a class="gris" href="(.*?)">(.*?)</a>\n'
-               '<a href="(.*?)">(.*?)</a>\n'
-               '<span class="txs gris italic">(.*?)</span>')
-    contributions_data = re.findall(pattern, txt)
-    if not contributions_data:  # Could not parse contributions data about a user
-        return None
-    contributions = []
-    for item in contributions_data:
-        path, name, path_solve, solve, date = item
-        name = name.replace('\t', '')[1:-1]
-        contribution = {
-            'path': path,
-            'name': unescape(name),
-            'path_solve': path_solve,
-            'solve': unescape(solve),
-            'date': unescape(date),
-        }
-        contributions.append(contribution)
-    return contributions
+def extract_contributions_page_numbers(content):
+    nb_challenges_pages, nb_solutions_pages = 0, 0
+    tree = html.fromstring(content)
+    div_elements = tree.xpath('//div[@class="t-body tb-padding"]/div')
+    h4_elements = [div_element.xpath('h4') for div_element in div_elements]
+    if not h4_elements:
+        return nb_challenges_pages, nb_solutions_pages
+
+    for div_element in div_elements:
+        title = div_element.xpath('h4/text()')[0]
+        pages = div_element.xpath('div[@class="pagination-centered"]/ul[@class="pagination"]/li')
+        if 'Challenge' in title:
+            if not pages:  # pages == []
+                nb_challenges_pages = 1
+            else:
+                nb_challenges_pages = len(pages)
+        if 'Solution' in title:
+            if not pages:  # pages == []
+                nb_solutions_pages = 1
+            else:
+                nb_solutions_pages = len(pages)
+    return nb_challenges_pages, nb_solutions_pages
+
+
+def extract_challenges_contributions(content):
+    tree = html.fromstring(content)
+    ul_elements = tree.xpath('//div[@class="t-body tb-padding"]/div/a[contains(@name, "challenge")]/parent::div/ul')
+    if not ul_elements:
+        return []
+    challenges = []
+    li_elements = ul_elements[0].xpath('li')
+    for li_element in li_elements:
+        category = li_element.xpath('a[1]/@href')[0].split('/')[2]
+        challenge_name = li_element.xpath('a[2]/text()')[0]
+        date = li_element.xpath('span/text()')[0].replace('\xa0', ' ')
+        challenges.append({'challenge_name': challenge_name, 'category': category, 'date': date})
+    return challenges
+
+
+def extract_solutions_contributions(content):
+    tree = html.fromstring(content)
+    ul_elements = tree.xpath('//div[@class="t-body tb-padding"]/div/a[contains(@name, "solution")]/parent::div/ul')
+    if not ul_elements:
+        return []
+    solutions = []
+    li_elements = ul_elements[0].xpath('li')
+    for li_element in li_elements:
+        challenge_name = li_element.xpath('a[1]/@href')[0].split('/')[-1]
+        solution_path = li_element.xpath('a[2]/@href')[0]
+        solution_title = li_element.xpath('a[2]/text()')[0]
+        date = li_element.xpath('span/text()')[0].replace('\xa0', ' ')
+        solution = {'challenge_name': challenge_name, 'solution_path': solution_path,
+                    'solution_title': solution_title, 'date': date}
+        solutions.append(solution)
+    return solutions
--- worker/worker/redis_interface/contributions.py
+++ worker/worker/redis_interface/contributions.py
@@ -1,10 +1,56 @@
+from functools import partial
+import itertools
 import json
-
 from worker import log
 from worker.constants import URL
-from worker.parser.contributions import extract_contributions
-from worker.parser.profile import extract_pseudo
+from worker.parser.contributions import extract_challenges_contributions, extract_solutions_contributions, \
+    extract_contributions_page_numbers
 from worker.redis_interface import session, redis_app
+from multiprocessing.pool import ThreadPool
+
+
+def get_challenge_contributions(username, page_index):
+    url = f'{URL}{username}?inc=contributions&debut_challenges_auteur={5 * page_index}#pagination_challenges_auteur'
+    r = session.get(url)
+    if r.status_code != 200:
+        log.warning(f'HTTP {r.status_code} for username {username}.')
+        return
+    return extract_challenges_contributions(r.content)
+
+
+def get_solution_contributions(username, page_index):
+    url = f'{URL}{username}?inc=contributions&debut_solutions_auteur={5 * page_index}#pagination_solutions_auteur'
+    r = session.get(url)
+    if r.status_code != 200:
+        log.warning(f'HTTP {r.status_code} for username {username}.')
+        return
+    return extract_solutions_contributions(r.content)
+
+
+def format_contributions_challenges(username, nb_challenges_pages):
+    #  Retrieve challenges contributions
+    challenges_contributions = []
+    if nb_challenges_pages == 0:
+        return challenges_contributions
+    tp_function = partial(get_challenge_contributions, username)
+    tp_argument = list(range(nb_challenges_pages))
+    with ThreadPool(nb_challenges_pages) as tp:
+        response_challenges = tp.map(tp_function, tp_argument)  # list of requests.content
+    challenges_contributions = list(itertools.chain(response_challenges))  # concatenate all challenges lists
+    return challenges_contributions
+
+
+def format_contributions_solutions(username, nb_solutions_pages):
+    #  Retrieve solutions contributions
+    solutions_contributions = []
+    if nb_solutions_pages == 0:
+        return solutions_contributions
+    tp_function = partial(get_solution_contributions, username)
+    tp_argument = list(range(nb_solutions_pages))
+    with ThreadPool(nb_solutions_pages) as tp:
+        response_solutions = tp.map(tp_function, tp_argument)  # list of requests.content
+    solutions_contributions = list(itertools.chain(response_solutions))  # concatenate all solutions lists
+    return solutions_contributions
 
 
 def set_user_contributions(username):
@@ -13,11 +59,21 @@ def set_user_contributions(username):
         log.warning(f'HTTP {r.status_code} for username {username}.')
         return
 
-    pseudo = extract_pseudo(r.text)
-    contributions = extract_contributions(r.text)
+    nb_challenges_pages, nb_solutions_pages = extract_contributions_page_numbers(r.content)
+    if nb_challenges_pages == 0 and nb_solutions_pages == 0:
+        return  # no challenges or solutions published by this user
+
+    challenges_contributions = format_contributions_challenges(username, nb_challenges_pages)
+    solutions_contributions = format_contributions_solutions(username, nb_solutions_pages)
 
     response = [{
-        'pseudo': pseudo,
-        'contributions': contributions,
+        'contributions': {
+            'challenges': challenges_contributions,
+            'solutions': solutions_contributions
+        }
     }]
+    if challenges_contributions is not None:
+        redis_app.set(f'{username}.contributions.challenges', json.dumps(challenges_contributions))
+    if solutions_contributions is not None:
+        redis_app.set(f'{username}.contributions.solutions', json.dumps(solutions_contributions))
     redis_app.set(f'{username}.contributions', json.dumps(response))
