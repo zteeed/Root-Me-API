1f1b9c875e5ebccf5f6fb00a7084f389a1993c43
zteeed <zteeed@minet.net>
2019-08-09 20:05:01 +0000

    Use redis default port

--- api/api/constants.py
+++ api/api/constants.py
@@ -2,4 +2,4 @@ AUTHORS = ["Aurélien Duboc", "Nicolas Bonnet"]
 GITHUB_ACCOUNTS = ["https://github.com/zteeed", "https://github.com/bonnetn"]
 VERSION = 'v2'
 REDIS_HOST = 'localhost'
-REDIS_PORT = 32768
+REDIS_PORT = 6379
