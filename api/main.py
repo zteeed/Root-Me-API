db67941227fcdd2c513fbde491003ae31db622a0
zteeed <zteeed@minet.net>
2019-08-09 20:19:49 +0000

    Improve code structure with pull requests comments (https://github.com/zteeed/Root-Me-API/pull/15\#issue-306077628)

--- api/main.py
+++ api/main.py
@@ -1,14 +1,19 @@
 import asyncio
+import aioredis
 
+from tornado.web import Application
 from tornado.options import define, options
 
-from api.app import Application
+from api.constants import REDIS_HOST, REDIS_PORT
+from api.handlers import handlers
 
 if __name__ == '__main__':
     define('port', default=3000, help='port to listen on')
-    application = Application()
+    application = Application(handlers)
     application.listen(options.port)
 
     loop = asyncio.get_event_loop()
-    application.init_with_loop(loop)
+    application.redis = loop.run_until_complete(
+        aioredis.create_redis_pool((REDIS_HOST, REDIS_PORT), loop=loop)
+    )
     loop.run_forever()
