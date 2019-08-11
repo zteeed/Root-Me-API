c11dd6372ba4f7391df5dc02b1d2d23ea50b173a
zteeed <zteeed@minet.net>
2019-08-11 01:56:16 +0000

    Use session requests in http_client script

--- worker/worker/http_client.py
+++ worker/worker/http_client.py
@@ -1,7 +1,20 @@
 from typing import Optional
 
+from requests import Session
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
+
 from worker import log
-from worker.redis_interface import session
+
+session = Session()
+retry = Retry(
+    total=10,
+    backoff_factor=1,
+    status_forcelist=[429],
+)
+adapter = HTTPAdapter(max_retries=retry, pool_maxsize=100, pool_block=True)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
 
 
 class HTTPBadStatusCodeError(RuntimeError):
