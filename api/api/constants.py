8db3939c0ff161a30b4d087686abfd013e803c9e
zteeed <zteeed@minet.net>
2019-09-15 23:59:47 +0000

    Create consumer groups when api starts

--- api/api/constants.py
+++ api/api/constants.py
@@ -3,3 +3,6 @@ GITHUB_ACCOUNTS = ["https://github.com/zteeed", "https://github.com/bonnetn"]
 VERSION = 'v2'
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
+REDIS_STREAM_USERS = 'update_users'
+REDIS_STREAM_CHALLENGES = 'update_challenges'
+CG_NAME = 'rootme'
---
+++ api/init.py
@@ -0,0 +1,22 @@
+import aioredis
+
+from api.constants import REDIS_STREAM_USERS, REDIS_STREAM_CHALLENGES, CG_NAME
+
+
+async def create_cg(redis_app, stream, group_name, latest_id='$', mkstream=False):
+    #  aioredis==1.2.0 install via pip does not support mkstream option on xgroup_create (see github repository)
+    args = [b'CREATE', stream, group_name, latest_id]
+    if mkstream:
+        args.append(b'MKSTREAM')
+    await redis_app.execute(b'XGROUP', *args)
+
+
+async def create_cgs(redis_app):
+    for stream in [REDIS_STREAM_CHALLENGES, REDIS_STREAM_USERS]:
+        try:
+            await create_cg(redis_app, stream, CG_NAME, mkstream=True)
+        except aioredis.errors.ReplyError as exception:
+            if 'BUSYGROUP Consumer Group name already exists' == str(exception):
+                pass
+            else:
+                raise exception
