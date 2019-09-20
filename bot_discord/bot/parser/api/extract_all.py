0445461fc83b3876aa820455a3804377f3e82a05
zteeed <zteeed@minet.net>
2019-09-20 01:55:06 +0000

    Fix bot request to API

--- bot_discord/bot/parser/api/extract_all.py
+++ bot_discord/bot/parser/api/extract_all.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, Optional
 
 import requests
 
-from bot.colors import purple, red
+from bot.colors import green, red
 from bot.constants import URL, timeout
 
 response_content_type = Optional[Dict[str, Any]]
@@ -11,17 +11,18 @@ response_content_type = Optional[Dict[str, Any]]
 
 def request_to(url: str) -> Optional[requests.models.Response]:
     try:
-        return requests.get(url, timeout=timeout)
+        return requests.get(url, verify=False, timeout=timeout)
     except Exception as exception:
         red(exception)
         return None
 
 
 def extract_json(url: str) -> response_content_type:
-    purple(url)
     r = request_to(url)
-    if r is None:
+    if r is None or r.status_code != 200:
+        red(url)
         return
+    green(url)
     data = json.loads(r.content.decode())
     if 'body' in data.keys():
         return data['body']
