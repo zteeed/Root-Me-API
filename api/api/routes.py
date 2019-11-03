8a214e645d8d35d67d299b71183c499787b4b80b
zteeed <zteeed@minet.net>
2019-11-03 01:59:42 +0000

    Delete version parameter + use lang parameter on handlers endpoints / redis keys

--- api/api/routes.py
+++ api/api/routes.py
@@ -1,15 +1,13 @@
-from api.constants import VERSION
-
 routes = {
-    'info': f'GET /{VERSION}',
-    'categories': f'GET /{VERSION}/categories',
-    'category_info': f'GET /{VERSION}/category/<string:category>',
-    'challenges': f'GET /{VERSION}/challenges',
-    'user_profile': f'GET /{VERSION}/<string:username>/profile',
-    'user_contributions': f'GET /{VERSION}/<string:username>/contributions',
-    'user_created_challenges': f'GET /{VERSION}/<string:username>/contributions/challenges',
-    'user_published_solutions': f'GET /{VERSION}/<string:username>/contributions/solutions',
-    'user_details': f'GET /{VERSION}/<string:username>/details',
-    'user_ctf_all_the_day_data': f'GET /{VERSION}/<string:username>/ctf',
-    'user_stats': f'GET /{VERSION}/<string:username>/stats',
+    'info': f'GET /',
+    'categories': f'GET /<string:lang>/categories',
+    'category_info': f'GET /<string:lang>/category/<string:category>',
+    'challenges': f'GET /<string:lang>/challenges',
+    'user_profile': f'GET /<string:lang>/<string:username>/profile',
+    'user_contributions': f'GET /<string:lang>/<string:username>/contributions',
+    'user_created_challenges': f'GET /<string:lang>/<string:username>/contributions/challenges',
+    'user_published_solutions': f'GET /<string:lang>/<string:username>/contributions/solutions',
+    'user_details': f'GET /<string:lang>/<string:username>/details',
+    'user_ctf_all_the_day_data': f'GET /<string:lang>/<string:username>/ctf',
+    'user_stats': f'GET /<string:lang>/<string:username>/stats',
 }
