f23c7c4bc31d77ddeddd7015d3cf4a4914b1d6a9
zteeed <zteeed@minet.net>
2019-11-03 01:56:13 +0000

    Remove URL last slash

--- worker/worker/constants.py
+++ worker/worker/constants.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-URL = 'https://www.root-me.org/'
+URL = 'https://www.root-me.org'
 REDIS_STREAM_USERS = 'update_users'
 REDIS_STREAM_CHALLENGES = 'update_challenges'
 CG_NAME = 'rootme'
