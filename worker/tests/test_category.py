9f3be5e323c231d21dcc5b637a6464078866d4c3
zteeed <zteeed@minet.net>
2019-11-04 05:59:07 +0000

    Add typing on worker tests

--- worker/tests/test_category.py
+++ worker/tests/test_category.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 
 from pytest import fixture
 
@@ -6,36 +7,36 @@ from worker.parser import category
 
 
 @fixture
-def category_page_html():
-    with open('fixtures/fr_category.html', 'r') as f:
+def category_page_html() -> bytes:
+    with open('fixtures/fr_category.html', 'rb') as f:
         return f.read()
 
 
 @fixture
-def category_app_system_html():
-    with open('fixtures/fr_category_app_system.html', 'r') as f:
+def category_app_system_html() -> bytes:
+    with open('fixtures/fr_category_app_system.html', 'rb') as f:
         return f.read()
 
 
 @fixture
-def category_app_system_result():
+def category_app_system_result() -> Dict:
     with open('fixtures/fr_category_app_system.json', 'r') as f:
         return json.loads(f.read())
 
 
 @fixture
-def category_cracking_html():
-    with open('fixtures/fr_category_cracking.html', 'r') as f:
+def category_cracking_html() -> bytes:
+    with open('fixtures/fr_category_cracking.html', 'rb') as f:
         return f.read()
 
 
 @fixture
-def category_cracking_result():
+def category_cracking_result() -> Dict:
     with open('fixtures/fr_category_cracking.json', 'r') as f:
         return json.loads(f.read())
 
 
-def test_extract_categories(category_page_html: str):
+def test_extract_categories(category_page_html: bytes):
     want = {'App-Script', 'App-Systeme', 'Cracking', 'Cryptanalyse', 'Forensic', 'Programmation',
             'Realiste', 'Reseau', 'Steganographie', 'Web-Client', 'Web-Serveur'}
 
@@ -44,10 +45,10 @@ def test_extract_categories(category_page_html: str):
 
 
 class TestExtractCategoryInfo:
-    def test_app_system(self, category_app_system_html: str, category_app_system_result: dict):
+    def test_app_system(self, category_app_system_html: bytes, category_app_system_result: Dict):
         actual = category.extract_category_info(category_app_system_html, 'App-Systeme')
         assert actual == category_app_system_result
 
-    def test_cracking(self, category_cracking_html: str, category_cracking_result: dict):
+    def test_cracking(self, category_cracking_html: bytes, category_cracking_result: Dict):
         actual = category.extract_category_info(category_cracking_html, 'Cracking')
         assert actual == category_cracking_result
